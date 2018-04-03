66db705e74e1e7e2969ad4719228259d8e9b42c4
vvas <vinedias@gmail.com>
2018-04-03 12:52:59 -0300

    1

--- GGmodel.py
+++ GGmodel.py
@@ -22,74 +22,13 @@ class GilmoreGomory:
        # >>> c.linear_constraints.get_rhs()
        # [0.0, 1.0, -1.0, 2.0]
        # """
-
-
-  def restricoes(self, prob, m_colnames, m_rhs, A, constraints):
-    m_rownames = ["demanda1", "demanda2", "demanda3", "demanda4"]
-    m_senses = ["G", "G", "G", "G"]
-    for i in range(len(m_rhs)):
-      constraints[i][0] = m_colnames
-      constraints[i][1] = A[i]
-    print(constraints)
-    prob.linear_constraints.add(lin_expr = constraints, senses = m_senses, rhs = m_rhs, names = m_rownames)
+       
     #prob.linear_constraints.add(lin_expr = constraints, senses = constraints_senses, rhs = m_rhs,  names = m_rownames)
     #prob.objective.set_sense(prob.objective.sense.minimize)
     #prob.variables.add(obj = m_obj, lb = m_lb, ub = m_ub, names = m_colnames)
     #for i in range(len(D)):
       #constraints.append(["x1", "x2"], [])
       #print("teste")
-
-
-  def padroesiniciais(self, m_colnames, m_obj, L, l, A):
-    N = 0
-    constraints = []
-    #aux = ""
-    for j in range(len(l)):
-      if j != N:
-        A[j][N] = 0
-      else: 
-        A[j][N] = np.floor(L/l[j])
-      #aux +=
-      #constraints.append([""])  
-      N += 1
-      m_colnames[j] = str("x" + str(j))
-      m_obj[j] = 1
-    print(m_colnames)
-      
-    #print(A)
-    
-   # prob.linear_constraints.add()
-
-  def __init__(self, prob):
-    print("inicio")
-
-  def addvariables(self, prob):
-    prob.variables.add(obj = m_obj, lb = m_lb, ub = m_ub, names = m_colnames)
-
-  
-
-print('testing')
-A = [[0 for x in range(4)] for y in range(4)]
-constraints = [[[0 for x in range(4)] for y in range(2)] for w in range(4)]
-m_colnames = ["0", "0", "0", "0"]
-m_obj = [0, 0, 0, 0]
-m_ub = [cplex.infinity, cplex.infinity, cplex.infinity, cplex.infinity]
-m_lb = [0, 0, 0, 0]
-L = 100
-l = [50, 40, 30, 15]
-D = [50, 50, 100, 100]
-corte = cplex.Cplex()
-mochila = cplex.Cplex()
-gg = GilmoreGomory(corte)
-gg.padroesiniciais(m_colnames, m_obj, L, l, A)
-gg.addvariables(corte)
-gg.restricoes(corte, m_colnames, D, A, constraints)
-corte.objective.set_sense(corte.objective.sense.minimize)
-print(corte.solve())
-print(corte.solution.get_values())
-
-
-
 
 #m_obj = [-2.0, -1.0]
 #m_ub = [cplex.infinity, cplex.infinity]
@@ -119,5 +58,63 @@ print(corte.solution.get_values())
 #  m_rownames += "Demanda" + str(i)
 
 #print(m_rownames)  
+
+
+
+
+  def restricoes(self, prob, m_colnames, m_rhs, A, constraints):
+    m_rownames = ["demanda1", "demanda2", "demanda3", "demanda4"]
+    m_senses = ["G", "G", "G", "G"]
+    for i in range(len(m_rhs)):
+      constraints[i][0] = m_colnames
+      constraints[i][1] = A[i]
+    print(constraints)
+    prob.linear_constraints.add(lin_expr = constraints, senses = m_senses, rhs = m_rhs, names = m_rownames)
+
+
+  def padroesiniciais(self, m_colnames, m_obj, L, l, A):
+    N = 0
+    constraints = []
+    #aux = ""
+    for j in range(len(l)):
+      if j != N:
+        A[j][N] = 0
+      else: 
+        A[j][N] = np.floor(L/l[j])
+      #aux +=
+      #constraints.append([""])  
+      N += 1
+      m_colnames[j] = str("x" + str(j))
+      m_obj[j] = 1
+    print(m_colnames)
+
+  def __init__(self, prob):
+    print("inicio")
+
+  def addvariables(self, prob):
+    prob.variables.add(obj = m_obj, lb = m_lb, ub = m_ub, names = m_colnames)
+
+  
+
+print('testing')
+A = [[0 for x in range(4)] for y in range(4)]
+constraints = [[[0 for x in range(4)] for y in range(2)] for w in range(4)]
+m_colnames = ["0", "0", "0", "0"]
+m_obj = [0, 0, 0, 0]
+m_ub = [cplex.infinity, cplex.infinity, cplex.infinity, cplex.infinity]
+m_lb = [0, 0, 0, 0]
+L = 100
+l = [50, 40, 30, 15]
+D = [50, 50, 100, 100]
+corte = cplex.Cplex()
+mochila = cplex.Cplex()
+gg = GilmoreGomory(corte)
+gg.padroesiniciais(m_colnames, m_obj, L, l, A)
+gg.addvariables(corte)
+gg.restricoes(corte, m_colnames, D, A, constraints)
+corte.objective.set_sense(corte.objective.sense.minimize)
+print(corte.solve())
+print(corte.solution.get_values())
+
 
 
