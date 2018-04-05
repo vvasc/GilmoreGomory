e8c29d50b26bc80f68b425005a28ec873a659835
vvas <vinedias@gmail.com>
2018-04-05 18:47:54 -0300

    problemas na inserção de dados na api

--- GGmodel.py
+++ GGmodel.py
@@ -3,7 +3,6 @@ from cplex.exceptions import CplexError
 import sys
 import numpy as np
 
-class GilmoreGomory:
 
   
   # para variaveis: 
@@ -39,7 +38,7 @@ class GilmoreGomory:
 #m_sense = "LL"
 #prob.objective.set_sense(prob.objective.sense.minimize)
 #prob.variables.add(obj = m_obj, lb = m_lb, ub = m_ub, names = m_colnames)
-#first_constraint = [["x1", "x2"], [1, 1.0]]
+
 #second_constraint = [["x1", "x2"], [1.0, 0]]
 #third_constraint = [["x1", "x2"], [0, 1.0]]
 #constraints = [first_constraint, second_constraint, third_constraint]
@@ -61,14 +60,14 @@ class GilmoreGomory:
 
 
 
+class PrimalGilmoreGomory:
 
   def restricoes(self, prob, m_colnames, m_rhs, A, constraints):
     m_rownames = ["demanda1", "demanda2", "demanda3", "demanda4"]
     m_senses = ["G", "G", "G", "G"]
     for i in range(len(m_rhs)):
-      constraints[i][0] = m_colnames
+      constraints[i][0] = m_colnames #first_constraint = [["x1", "x2"], [1, 1.0]]
       constraints[i][1] = A[i]
-    print(constraints)
     prob.linear_constraints.add(lin_expr = constraints, senses = m_senses, rhs = m_rhs, names = m_rownames)
 
 
@@ -86,7 +85,7 @@ class GilmoreGomory:
       N += 1
       m_colnames[j] = str("x" + str(j))
       m_obj[j] = 1
-    print(m_colnames)
+   
 
   def __init__(self, prob):
     print("inicio")
@@ -96,11 +95,39 @@ class GilmoreGomory:
 
   
 
+
+class DualGilmoreGomory:
+  
+  def addvariables(self, prob):
+    prob.variables.add(obj = m_obj, lb = m_lb, ub = D, names = m_colnames)
+
+  def __init__(self, prob):
+    print("iniciomochila")
+
+  def mochilainicio(self, m_colnames, l):
+    for j in range(len(l)):
+      m_colnames[j] = str("a"+ str(j))
+
+
+  def restricoes(self, prob, m_colnames, m_rhs, l, constraints):
+    m_rownames = ["existencia1", "existencia2", "existencia3", "existencia4"]
+    m_senses = ["L", "L", "L", "L"]
+    print(m_rhs)
+    print(m_colnames)
+    print(M)
+    for i in range(len(M)):
+      constraints[i][0] = m_colnames #first_constraint = [["x1", "x2"], [1, 1.0]]
+      constraints[i][1] = l
+    print(constraints)
+    prob.linear_constraints.add(lin_expr = constraints, senses = m_senses, rhs = m_rhs, names = m_rownames)
+  
+
 print('testing')
 A = [[0 for x in range(4)] for y in range(4)]
 constraints = [[[0 for x in range(4)] for y in range(2)] for w in range(4)]
 m_colnames = ["0", "0", "0", "0"]
 m_obj = [0, 0, 0, 0]
+m_rhs = [0, 0, 0, 0]
 m_ub = [cplex.infinity, cplex.infinity, cplex.infinity, cplex.infinity]
 m_lb = [0, 0, 0, 0]
 L = 100
@@ -108,13 +135,22 @@ l = [50, 40, 30, 15]
 D = [50, 50, 100, 100]
 corte = cplex.Cplex()
 mochila = cplex.Cplex()
-gg = GilmoreGomory(corte)
+gg = PrimalGilmoreGomory(corte)
+mo = DualGilmoreGomory(mochila)
 gg.padroesiniciais(m_colnames, m_obj, L, l, A)
 gg.addvariables(corte)
 gg.restricoes(corte, m_colnames, D, A, constraints)
 corte.objective.set_sense(corte.objective.sense.minimize)
 print(corte.solve())
 print(corte.solution.get_values())
+M = corte.solution.get_dual_values()
+constraints = [[[0 for x in range(4)] for y in range(2)] for w in range(4)]
+mo.mochilainicio(m_colnames, l)
+for i in range(len(M)):
+  m_rhs[i] = L
+mo.restricoes(mochila, m_colnames, m_rhs, l, constraints)
+
+
 
 
 
